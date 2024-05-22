0e9f384c7fe0aeaec51d778c67df63f6d37ace65
Xayjo <ximena.camachovadela@gmail.com>
2024-05-21 22:24:13 -0300

    agregando datos de clase

---
+++ clase46/forOf.js
@@ -0,0 +1,18 @@
+let nombresRandom = [
+    { nombre: "Alpha" },
+    { nombre: "Bravo" },
+    { nombre: "Charlie" },
+    { nombre: "Delta" },
+    { nombre: "Echo" },
+    { nombre: "Foxtrot" },
+    { nombre: "Golf" },
+    { nombre: "Hotel" },
+    { nombre: "India" },
+    { nombre: "Juliet" }
+];
+
+let numerosDesordenados = [5, 2, 8, 9, 3, 7, 1, 6, 10, 4];
+
+for (const nombreRandom of nombresRandom) {
+    console.log(nombreRandom.nombre);
+}
